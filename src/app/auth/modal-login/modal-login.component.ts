98e5485b4d1c4434e7af314b183f22998ffb6f88
sofiagaona <sofia.herrera.ga@gmail.com>
2022-01-12 11:28:51 -0600

    Crea funcionalidad para login

--- src/app/auth/modal-login/modal-login.component.ts
+++ src/app/auth/modal-login/modal-login.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-
+import { Auth } from '@angular/fire/auth';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
+import { User } from '../auth';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-modal-login',
@@ -9,15 +15,57 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class ModalLoginComponent implements OnInit {
   
+public user!:User
+
+  formLogin: FormGroup= this.fb.group({
+    email: ['',[Validators.required, Validators.email]],
+    password: ['',[Validators.required,Validators.minLength(6)]]
+  })
+
   constructor(  
     public activeModal: NgbActiveModal,
-  
+    private auth:Auth,
+    private fb:FormBuilder,
+    private authService:AuthService,
+    private router:Router
  
     ) {} 
  closeModal(send:any){
  this.activeModal.close(send );
  }
-  
+  login(){
+    const {email, password} = this.formLogin.value;
+     this.authService.login(email,password)
+     .then ( resp => {
+
+          
+            this.user = {
+              email:resp.user.email!,
+            }
+            this.authService._user=this.user
+            console.log(this.user)
+            //localStorage.setItem('user',JSON.stringify(this.authService.user) );
+            this.router.navigateByUrl('./categories')
+          
+          
+        })
+        .catch(error=>{
+          Swal.fire({
+            title: 'Error!',
+            text: 'Credenciales invalidas',
+            confirmButtonText: 'OK'
+          })
+        })
+
+  // .then(resp=> {
+  //   console.log(this.authService.user)
+  //     this.router.navigateByUrl('./categories')
+      
+  //   }
+      
+    
+    
+  }
   ngOnInit(): void {
   }
 
